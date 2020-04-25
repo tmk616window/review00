c5d48871e6c444dc43ba1f64806b1ea9dbec78ca
EC2 Default User <ec2-user@ip-172-31-34-66.ap-northeast-1.compute.internal>
2020-04-25 22:24:35 +0000

    search finish

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :fog
 
   # developmentとtest以外はS3を使用
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
     storage :fog
+  end
   # 画像ごとに保存するディレクトリを変える
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
