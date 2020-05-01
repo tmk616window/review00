159bfa13738f8665db6e21e6cb36f0e8265cb383
EC2 Default User <ec2-user@ip-172-31-34-66.ap-northeast-1.compute.internal>
2020-05-01 12:38:03 +0000

    chat second

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -10,11 +10,17 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-  var node = document.createElement("p"); 
-
+    var node2 = document.createElement("br"); 
+    var node1 = document.createElement("h6"); 
+    var node = document.createElement("p");
+    var textnode1 = document.createTextNode('box'); 
     var textnode = document.createTextNode(data.content.content); 
+    node2.appendChild(textnode); 
+    node1.appendChild(textnode1); 
     node.appendChild(textnode); 
     document.getElementById("new_message").appendChild(node);
+    document.getElementById("new_message").appendChild(node1);
+    document.getElementById("new_message").appendChild(node2);
     document.getElementById('chat_message1').value= ''
     // Called when there's incoming data on the websocket for this channel
   }
