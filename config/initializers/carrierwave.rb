956828aa7a2c913fbba86f627566471066c15f15
tmk616window <tmk616window@yahoo.co.jp>
2020-05-02 06:06:24 +0000

    image commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,18 +2,25 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-  CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: '',
-    aws_secret_access_key: '',
-    region: 'ap-northeast-1',
-    path_style: true
-  }
 
-    config.fog_directory  = 'rails-photo-123'
-    config.cache_storage = :fog
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'tmk616window'
+    config.asset_host = 'https://s3.amazonaws.com/tmk616window'
+    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+    config.fog_public = false # ←コレ
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: ENV['S3_REGION'],
+      # path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
-  
-  
+end
   
